8365a6fe3491e040fcbda3a10c72f0bc26d62601
robertDurst <me@robdurst.com>
2022-03-31 12:58:05 -0400

    checkboxes for optimizations

--- app/controllers/gia_controller.rb
+++ app/controllers/gia_controller.rb
@@ -1,7 +1,16 @@
 class GiaController < ApplicationController
   def disasm
     input = params[:description]
+    optimizations = params[:optimizations]
+   
     begin
+      compile_options_h = {}
+      compile_options_a = optimizations.split(',').each do |op|
+        op_name, is_on = op.split(':')
+        compile_options_h[op_name.to_sym] = is_on == 'true'
+      end
+  
+      RubyVM::InstructionSequence.compile_option = compile_options_h
       insns = RubyVM::InstructionSequence.compile(input).disasm
       some_text = {
         success: true,
--- app/javascript/react/src/components/Gia.jsx
+++ app/javascript/react/src/components/Gia.jsx
@@ -8,13 +8,23 @@ class Hello extends React.Component {
 
     this.state = {
       rubyText: "",
-      disasmText: ""
+      disasmText: "",
+      optimizations: {
+        inline_const_cache: false,
+        instructions_unification: false,
+        peephole_optimization: false,
+        specialized_instruction: false,
+        stack_caching: false,
+        tailcall_optimization: false
+      }
     }
   }
 
-  handleRubyTextChange = (e) => {
-    this.setState({rubyText: e.target.value});
-    const url = "/gia/disasm?description=" + encodeURIComponent(e.target.value);
+  updateDisasm(currentRubyText, currentOptimization) {
+    const url = "/gia/disasm?description=" +
+      encodeURIComponent(currentRubyText) + 
+      "&optimizations=" + 
+      encodeURIComponent(Object.keys(currentOptimization).map(ob => ob + ":" + currentOptimization[ob]).join(','));
     fetch(url)
       .then(response => {
         if (response.ok) {
@@ -25,16 +35,43 @@ class Hello extends React.Component {
       .then(response => this.setState({ disasmText: response.disasmText }))
   }
 
+  handleRubyTextChange = (e) => {
+    this.setState({rubyText: e.target.value});
+    this.updateDisasm(e.target.value, this.state.optimizations);
+  }
+
+  handleCheckbox = (e) => {
+    let tempOptimizations = this.state.optimizations;
+    tempOptimizations[e.target.name] = e.target.checked;
+    this.setState({optimizations: tempOptimizations})
+    this.updateDisasm(this.state.rubyText, tempOptimizations);
+  }
+
+  
 
   render () {
     return (
-      <div id="ide_container">
-        <h1 id="title_container">Shipley's Gia Inspect</h1>
-        <div class="text_form_box_container">
-          <textarea class="text_form_box" id="myform_inputbox" value={this.state.rubyText} onChange={this.handleRubyTextChange}/>
-          <textarea class="text_form_box" id="disasm_box" value={this.state.disasmText}/>
+      <div id="full_view_container">
+        <div id="ide_container">
+          <h1 id="title_container">Shipley's Gia Inspect</h1>
+          <div class="text_form_box_container">
+            <textarea class="text_form_box" id="myform_inputbox" value={this.state.rubyText} onChange={this.handleRubyTextChange}/>
+            <textarea class="text_form_box" id="disasm_box" value={this.state.disasmText}/>
+          </div>
         </div>
-    </div>
+        <div id="compiler_options_container_container">
+          <div id="compiler_options_container">
+            <h3>Compiler Options: </h3>
+            {
+              Object.keys(this.state.optimizations).map(optimizationName => {
+                return <div class="compiler_option">
+                  <input type="checkbox" name={optimizationName} value={optimizationName} onChange={this.handleCheckbox} checked={this.state.optimizations[optimizationName]}/> {optimizationName}
+                </div>
+              })
+            }
+          </div>
+        </div>
+      </div>
     );
   }
 }
