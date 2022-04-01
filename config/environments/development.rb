12a297cce9ceb1fcf0a88b19df371d2a04c435c9
robertDurst <me@robdurst.com>
2022-04-01 14:51:21 -0400

    attempt to fix hosts issue with heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Fix Heroku deploy issue
+  config.hosts << "shipley-gia.herokuapp.com"
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
